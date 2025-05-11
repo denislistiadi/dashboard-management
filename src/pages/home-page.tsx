640a3f14f5503c7e7e742f7a3e45d590b29581f0
Denis Listiadi <denislistiadi24@gmail.com>
2025-05-11 15:16:03 +0700

    feat: update HomePage layout to include LineChartDashboard and DonutChartDashboard components

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,18 +1,22 @@
+import { LineChartDashboard } from "@/containers/linechart-dashboard";
 import AppTitle from "@/components/app-title";
 import { Progress } from "@/components/ui/progress";
 import CardDashboard from "@/containers/card-dashboard";
+import { DonutChartDashboard } from "@/containers/donutchart-dashboard";
 
 function HomePage() {
   return (
     <div className="flex flex-col gap-6 w-full">
       <AppTitle title="Dashboard" />
-      <div className="grid grid-cols-4 gap-6 w-full">
+      <div className="grid grid-cols-12 gap-6 w-full">
         <CardDashboard title="Total Domain" value={20}></CardDashboard>
         <CardDashboard title="Total Mailbox" value={120}></CardDashboard>
         <CardDashboard title="Total Storage">
           <Progress value={40} className="my-3" aria-label="progres-storage" />
         </CardDashboard>
         <CardDashboard title="Service Status" value={"Active"}></CardDashboard>
+        <LineChartDashboard />
+        <DonutChartDashboard />
       </div>
     </div>
   );
