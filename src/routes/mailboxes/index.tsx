929ecf1af2eb2a0f34fa4ac488e81b806f978811
Denis Listiadi <denislistiadi24@gmail.com>
2025-05-13 09:46:23 +0700

    feat: update mailboxes route to use MailboxPage component

--- src/routes/mailboxes/index.tsx
+++ src/routes/mailboxes/index.tsx
@@ -1,10 +1,7 @@
-import { ROUTE_MAILBOXES } from '@/constants/route.constant'
-import { createFileRoute } from '@tanstack/react-router'
+import { ROUTE_MAILBOXES } from "@/constants/route.constant";
+import MailboxPage from "@/pages/mailbox-page";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute(ROUTE_MAILBOXES)({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/mailboxes/"!</div>
-}
+  component: MailboxPage,
+});
