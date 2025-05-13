0992733978bda592686f4a80819fe78b6016851b
Denis Listiadi <denislistiadi24@gmail.com>
2025-05-13 09:47:11 +0700

    perf: add ViteImageOptimizer plugin and configure manual chunks for build

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,32 @@ import tailwindcss from "@tailwindcss/vite";
 
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+    ViteImageOptimizer({ png: { quality: 5 } }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
 });
