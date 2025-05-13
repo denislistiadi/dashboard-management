2d61011f0ec64febf680545b21bdd46b2f74701b
Denis Listiadi <denislistiadi24@gmail.com>
2025-05-13 09:46:46 +0700

    feat: replace RouteComponent with SecurityPage in security route

--- src/routes/security/index.tsx
+++ src/routes/security/index.tsx
@@ -1,10 +1,8 @@
 import { ROUTE_SECURITY } from "@/constants/route.constant";
+import SecurityPage from "@/pages/security-page";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute(ROUTE_SECURITY)({
-  component: RouteComponent,
+  component: SecurityPage,
+  
 });
-
-function RouteComponent() {
-  return <div>Hello "/security/"!</div>;
-}
